c334b41cb57811fceb35f98ab1f1557a49abfd8c
Ljubomir Bojadziev <ljubomir.bojadziev@keitaro.com>
2020-12-20 21:28:49 +0100

    Refactor App and implement bootstrap,redux in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { rootReducer } from './store/reducers/rootReducer';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const store = createStore(rootReducer);
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
